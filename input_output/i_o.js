be53aa3eff8972f386ac79e4771807e9c56351d7
illialaz <illia.laziuk2@gmail.com>
2020-07-27 16:32:37 +0300

    Standart input - output

---
+++ input_output/i_o.js
@@ -0,0 +1,11 @@
+const { stderr, addListener } = require('process');
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+readline.question(`What's your name? `, (name) => {
+  console.log(`Hi, ${name}!`)
+  readline.close() 
+})
