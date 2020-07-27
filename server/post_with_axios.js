496b6c1f4d1c49a808f1f66810f52b335571d5b4
illialaz <illia.laziuk2@gmail.com>
2020-07-27 16:33:59 +0300

    Start your own server

---
+++ server/post_with_axios.js
@@ -0,0 +1,12 @@
+const axios = require('axios')
+
+axios.post('https://flaviocopes.com/todos', {
+  todo: 'Buy the milk'
+})
+.then((res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+  console.log(res)
+})
+.catch((error) => {
+  console.error(error)
+})
